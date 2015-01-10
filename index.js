c99f5c6e2677cc2e0d6d21249a42220897b6f4f9
Finn <derfinn@gmail.com>
2015-01-10 03:23:20 +0100

    merging

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var http = require('http')
+var fs = require('fs')
 var createHandler = require('github-webhook-handler')
 var request = require('request')
 var path = require('path')
@@ -10,8 +11,6 @@ var PORT = process.env['PORT'] || 8080
 var SECRET = process.env['SECRET'] || 'default'
 var ghtoken = process.env['GITHUB_TOKEN']
 var ghname = 'dat-bot'
-
-var repo = 'test'
 
 var ghheaders = {
   'user-agent': 'dat-bot',
@@ -30,22 +29,23 @@ http.createServer(function(req, res) {
 
 webhook.on('issue_comment', function (data) {
   console.log('event: issue_comment')
+  // console.log(data.payload)
   var comment = data.payload.comment.body
   var pos = comment.indexOf('@' + ghname)
   if(pos === -1) return // not for me
   comment = comment.slice(pos + ghname.length + 1)
   var issueUrl = data.payload.comment.issue_url
-  var username = data.payload.comment.user.login
+  var user = data.payload.comment.user
+  var username = user.login
 
   var match = comment.match(/publish (major|minor|patch)/)
   if(match) {
     var versionStep = match[1]
-    publishNewVersion(versionStep, postComment)
-  } else if(comment.match(/cat/)) {
-    postComment(cats())
-  } else {  
-    postComment('Hello, my friend!')
-  }
+    return publishNewVersion(data, versionStep, postComment)
+  }  
+  if(comment.match(/cat/)) {
+    return postComment(cats())
+  } 
   
   function postComment(message) {
     // post comment
@@ -60,15 +60,52 @@ webhook.on('issue_comment', function (data) {
   }
 })
 
-function publishNewVersion(versionStep, comment) {
-  var cwd = path.join(__dirname, repo)
-  var cmd = path.join(__dirname, 'publish.sh') + ' ' + versionStep
-  exec(cmd, {cwd: cwd}, donePublish)
-  var message
+function mdcode(test) {
+  return '\n```\n' + test + '\n```\n'
+}
+
+function publishNewVersion(data, versionStep, comment) {
+  var repo = data.payload.repository.full_name
+  var repoDir = path.join(__dirname, 'repos', repo)
+  fs.exists(repoDir, function (exists) {
+    if(exists) return checkPR()
+    var cloneURL = data.payload.repository.clone_url
+    
+    exec(['git clone ', cloneURL, repoDir].join(' '), {cwd: __dirname}, function (err, stdout, stderr) {
+      if(err) return comment('There has been a problem cloning the directory' + mdcode(stderr))
+      console.log(stdout)
+      // checks for publishing rights and push rights?
+      checkPR()
+    })
+    
+  })
+  
+  function checkPR() {
+    var pr = data.payload.issue.pull_request
+    if(pr) {
+      var mergeUrl = pr.url + '/merge'
+      console.log('Trying to merge ' + mergeUrl)
+      request.put({url: mergeUrl, headers: ghheaders, json: {}}, function (err, res, body) {
+        if(err) return comment('Oops there was a problem: ' + err)
+        if(!body.merged) return comment('I could not merge this, because GitHub said: ' + body.message)
+        runPublish()
+      })
+    } else {
+      runPublish()
+    }
+  }
+  
+  function runPublish() {
+    var cmd = path.join(__dirname, 'publish.sh') + ' ' + versionStep
+    exec(cmd, {cwd: repoDir}, donePublish)
+  }
+  
+  
   function donePublish(err, stdout, stderr) {
-  console.log(stdout)
+    console.log(stdout)
     if(err) {
-      message = 'Sorry, it seemed like there was a problem.\n'
+      var message
+      message = 'Sorry, it seems like there was a problem.\n'
       message += 'Here is the stderr output:\n```\n' + stderr + '\n```\n'
       comment(message)
     } else {
--- readme.md
+++ readme.md
@@ -1,3 +1,9 @@
 # dat-bot
 
 Bot that you can tell to publish modules, and maybe other things as well.
+
+## envs
+
+* `PORT` port to listen for github hooks
+* `SECRET` secret for github hooks
+* `GITHUB_TOKEN` Github token for the bot
